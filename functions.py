a57df2d66a0695e1371182272ff702ccc006e570
Dmitry-Peskov <dv.peskov@yandex.ru>
2024-08-03 21:34:21 +0300

    Добавил функцию поэлементного умножения некоторого свойства на массив весов

--- functions.py
+++ functions.py
@@ -1,3 +1,4 @@
+import numpy as np
 from typing import Sequence
 
 
@@ -14,3 +15,20 @@ def weighted_amount(
     """
     assert (len(features) == len(weights)), "Количество элементов в features и weights должно быть одинаковым"
     return sum([features[idx] * weights[idx] for idx in range(len(features))])
+
+
+def element_multiplication(
+        features: int | float,
+        weights: Sequence[int | float]
+) -> np.ndarray:
+    """
+    Произвести поэлементное умножение некоторого свойства, на каждый вес из массива weights
+
+    :param features: некоторое свойство
+    :param weights: массив содержащий веса
+    :return: массив произведений свойства на вес
+    """
+    output = np.array([0 for _ in range(len(weights))])
+    for idx in range(len(weights)):
+        output[idx] = features * weights[idx]
+    return output
